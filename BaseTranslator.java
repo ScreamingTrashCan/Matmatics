4e4d68538290582f611402df7156ea8708733b1d
ThatOneToaster <154563342+ScreamingTrashCan@users.noreply.github.com>
2026-01-30 13:11:22 -0700

    Base Translator
    Added an in-terminal simple program to change bases of input and output numbers.

---
+++ BaseTranslator.java
@@ -0,0 +1,227 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BaseTranslator {
+
+    // Intialize Characters
+    public Character[] letters = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+
+    private String input;
+    private String output;
+
+    private int inputBase;
+    private int outputBase;
+
+
+    public BigInteger base10Number;
+
+    public List<BigInteger> digitValues;
+    public List<Integer> digitAmounts;
+
+    // ------------------------
+    // Create Base Translator
+    // ------------------------
+    public BaseTranslator()
+    {
+        input = "";
+        output = "";
+        inputBase = 10;
+        outputBase = 10;
+        base10Number = BigInteger.ZERO;
+    }
+
+    // ------------------------
+    // Calculate
+    // ------------------------
+    public void calculate()
+    {
+        if (inputBase >= 2 && inputBase <= 36 && outputBase >=2 && outputBase <= 36)
+        {
+            base10Number = Normalize(input, inputBase);
+            if (!base10Number.equals(BigInteger.valueOf(-999)) && !base10Number.equals(BigInteger.valueOf(-1)) && !base10Number.equals(BigInteger.valueOf(-67)))
+            {
+
+            AddDigitValues(base10Number, outputBase);
+            AmountOfDigits(base10Number);
+
+            output = FinalizeString(base10Number);
+            }
+            else if (base10Number.equals(BigInteger.valueOf(-999)))
+            {
+                output = "Error! Not a valid character.";
+            }
+            else if (base10Number.equals(BigInteger.valueOf(-1)))
+            {
+                output = "Error! Cannot be a negative number.";
+            }
+            else if (base10Number.equals(BigInteger.valueOf(-67)))
+            {
+                output = "Error! Used a character with a higher value than the input base.";
+            }
+            else
+            {
+                output = "Error! Something went wrong :(";
+            }
+            }
+        else if (inputBase < 2 || inputBase > 36)
+        {
+            output = "Error! Input Base must be in the range of 2 - 36.";
+        }
+        else
+        {
+            output = "Error! Output Base must be in the range of 2 - 36.";
+        }
+    }
+
+
+    // ------------------------
+    // Convert character to a Base 10 Integer
+    // ------------------------
+    public Long BaseTenValue(char c)
+    {
+        if (c == '-')
+        {
+            return -1L;
+        }
+        else if (!Arrays.asList(letters).contains(c))
+        {
+            return -999L;
+        }
+            for (int i = 0; i < letters.length; i++)
+            {
+                if (letters[i] == c)
+                {
+                    return (long)i;
+                }
+            }
+        return 0L;
+    }
+
+    // ------------------------
+    // Operations
+    // ------------------------
+    String FinalizeString(BigInteger input)
+    {
+        String out = "";
+        for(int i = digitAmounts.size() -1; i >= 0; i--)
+        {
+            out += letters[(int)digitAmounts.get(i)];
+        }
+        return out;
+    }
+
+    void AmountOfDigits(BigInteger input)
+    {
+        BigInteger number = input;
+        for(int i = digitValues.size() - 1; i >= 0; i--)
+        {
+            while(number.compareTo(digitValues.get(i)) >= 0)
+            {
+                number = number.subtract(digitValues.get(i));
+                digitAmounts.set(i, digitAmounts.get(i) + 1);
+            }
+        }
+    }
+
+    BigInteger Normalize(String input, int inBase)
+    {
+        BigInteger out = BigInteger.ZERO;
+        int length = input.length();
+        for(int i = length - 1; i >= 0; i--)
+        {
+            int pow = length -i -1;
+            long num = BaseTenValue(input.charAt(i));
+            if(num == -999)
+            {
+                return BigInteger.valueOf(-999);
+            }
+            else if(num == -1)
+            {
+                return BigInteger.valueOf(-1);
+            }
+            else if(num > inputBase)
+            {
+                return BigInteger.valueOf(-67);
+            }
+            BigInteger power = BigInteger.valueOf((int)Math.pow(inBase, pow));
+            BigInteger adjustedNum = power.multiply(BigInteger.valueOf(num));
+            out = out.add(adjustedNum);
+        }
+        return out;
+    }
+
+    void AddDigitValues(BigInteger num, int baseInt)
+    {
+        digitValues = new ArrayList<>();
+        digitAmounts = new ArrayList<>();
+        BigInteger current = BigInteger.ZERO;
+        for(int i = 0; current.compareTo(num) <= 0; i++)
+        {
+            current = BigInteger.valueOf((int)Math.pow(baseInt, i));
+            if(current.compareTo(num) <= 0)
+            {
+                digitValues.add(current);
+                digitAmounts.add(0);
+            }
+        }
+    }
+
+    // ------------------------
+    // To String
+    // ------------------------
+    @Override
+    public String toString()
+    {
+        String str = "";
+        str += "+--------BASE TRANSLATOR INFO--------+\n";
+        str += "| Current Input: " + input + " /  Input Base: " + inputBase + "\n";
+        str += "| Current Output: " + output + " / Output Base: " + outputBase + "\n";
+        str += "+------------------------------------+";
+
+        return str;
+    }
+
+    
+    // ------------------------
+    // Getter and Setter
+    // ------------------------
+    public String getInput()
+    {
+        return input;
+    }  
+    public void setInput(String input)
+    {
+        this.input = input;
+    } 
+    
+    public String getOutput()
+    {
+        return output;
+    }
+    public void setOutput(String output)
+    {
+        this.output = output;
+    }
+
+    public int getInBase()
+    {
+        return inputBase;
+    }
+    public void setInBase(int inputBase)
+    {
+        this.inputBase = inputBase;
+    }
+
+    public int getOutBase()
+    {
+        return outputBase;
+    }
+    public void setOutBase(int outputBase)
+    {
+        this.outputBase = outputBase;
+    }
+
+}
---
+++ Runner.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class Runner {
+    
+    public static void main(String[] args) {
+        BaseTranslateMenu();
+    }
+
+    public static void BaseTranslateMenu()
+    {
+        BaseTranslator bT = new BaseTranslator();
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("BASE TRANSLATOR INPUT COMMAND: \n>calculate \n>set \n>info \n>exit");
+
+        String sLine = s.nextLine();
+
+        while(!sLine.equals("exit"))
+        {
+            switch(sLine)
+            {
+                case "calculate":
+                    bT.calculate();
+                    System.out.println("|| OUTPUT: " + bT.getOutput() + " ||");
+                    break;
+                case "set":
+                    System.out.println("   What would you like to change? \n   >input \n   >output \n   >inbase \n   >outbase");
+                    sLine = s.nextLine();
+                    System.out.println("      Choose value:");
+                    switch(sLine)
+                    {
+    
+                        case "input":
+                            bT.setInput(s.nextLine());
+                            break;
+                        case "output":
+                            bT.setOutput(s.nextLine());
+                            break;
+                        case "inbase":
+                            try {
+                                bT.setInBase(Integer.parseInt(s.nextLine()));
+                            }
+                            catch (Exception e){
+                                System.out.println("Error: Could not complete operation. " + e);
+                            }
+                            break;
+                        case "outbase":
+                            try {
+                                bT.setOutBase(Integer.parseInt(s.nextLine()));
+                            }
+                            catch (Exception e){
+                                System.out.println("Error: Could not complete operation. " + e);
+                            }
+                            break;
+                        default:
+                            System.out.println("Unknown command.");
+                    }
+                    break;
+                case "info":
+                    System.out.println(bT.toString());
+                    break;
+                default:
+                    System.out.println("Unknown command.");
+                    break;
+            }
+
+            System.out.println("INPUT COMMAND: \n>calculate \n>set\n>info \n>exit");
+
+            sLine = s.nextLine();
+        }
+
+        s.close();
+    }
+
+}
