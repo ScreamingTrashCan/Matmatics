59d0f4e7eaed2bd67f64f959aa3b56b1010826d3
WyattThompson-MTCHS <wyattT.2027@mtchs.org>
2026-02-02 10:31:25 -0700

    Added the custom font.

--- BaseTranslatorGUI.java
+++ BaseTranslatorGUI.java
@@ -1,19 +1,25 @@
 import java.awt.BorderLayout;
-import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.*;
 
 public class BaseTranslatorGUI extends JPanel {
-    private Font titleFont = new Font("Verdana", Font.BOLD, 48);
+    private Font saira;
+    private Font sairaBold;
     
     public BaseTranslatorGUI()
     {
         this.setPreferredSize(new Dimension(1920, 1080));
         this.setLayout(new BorderLayout());
         this.setBackground(Color.BLACK);
+
+        registerFonts();
 
         JPanel mainPanel = new JPanel();
         mainPanel.setBackground(new Color(40, 40, 40));
@@ -26,8 +32,27 @@ public class BaseTranslatorGUI extends JPanel {
 
         JLabel titleLabel = new JLabel("Base Translator");
         titleLabel.setForeground(Color.WHITE);
-        titleLabel.setFont(titleFont);
+        titleLabel.setFont(sairaBold);
         titleLabel.setHorizontalAlignment(JLabel.CENTER);
         headerPanel.add(titleLabel);
+    }
+
+    private void registerFonts()
+    {
+        try
+        {
+            saira = Font.createFont(Font.TRUETYPE_FONT, new File("Media/Fonts/Saira.ttf")).deriveFont(Font.BOLD, 36f);
+            sairaBold = Font.createFont(Font.TRUETYPE_FONT, new File("Media/Fonts/Saira-Bold.ttf")).deriveFont(Font.BOLD, 36f);
+            GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(saira);
+            GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(sairaBold);
+        }
+        catch (FontFormatException e)
+        {
+            System.out.println("Font Format Exception: " + e.getMessage());
+        }
+        catch (IOException e)
+        {
+            System.out.println("IO Exception: " + e.getMessage());
+        }
     }
 }
