d8f5ba61bbf2396863fca15eeea7573afe86e3c8
WyattThompson-MTCHS <wyattT.2027@mtchs.org>
2026-02-01 19:49:20 -0700

    Changed BaseTranslatorGUI.

--- BaseTranslatorGUI.java
+++ BaseTranslatorGUI.java
@@ -3,23 +3,10 @@ import java.awt.Dimension;
 
 import javax.swing.*;
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-public class BaseTranslatorGUI {
-    
-    // test
-
-=======
-=======
->>>>>>> Stashed changes
 public class BaseTranslatorGUI extends JPanel {
     public BaseTranslatorGUI()
     {
         this.setPreferredSize(new Dimension(1920, 1080));
         this.setLayout(new CardLayout());
     }
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 }
