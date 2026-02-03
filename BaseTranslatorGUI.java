f73cf3e76631f76a63fe11c496a50b042a86a67a
Wyatt-Thompson <wyatt.thompson2008@outlook.com>
2026-02-02 21:26:08 -0700

    Removed Unused Imports

--- BaseTranslatorGUI.java
+++ BaseTranslatorGUI.java
@@ -1,8 +1,6 @@
-import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.GraphicsEnvironment;
