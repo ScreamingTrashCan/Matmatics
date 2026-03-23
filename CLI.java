c790138f8a3ff84db6df72ae27604c404d563f2c
ThatOneToaster <154563342+ScreamingTrashCan@users.noreply.github.com>
2026-03-23 09:43:28 -0600

    Base Translator CLI
    Added Base Translator functionality to the CLI

--- CLI.java
+++ CLI.java
@@ -31,20 +31,22 @@ public class CLI {
             println("(3) Exit\n", "default");
             mainInput = scanChoice(new ArrayList<>(Arrays.asList(1, 2, 3))); // Takes in possible integer values
             switch (mainInput) {
-                case 1: // Calculator
+                case 1:
+                    // ----------
+                    // Calculator
+                    // ----------
                     input = 0;
                     println("Welcome to the calculator interface! Choose an option:", "blue");
                     println("(1) Calculate an Equation", "default");
                     println("(2) Exit\n", "default");
 
                     while (input != 2) {
-                        println("\nChoose an option:", "default");
+                        println("\nChoose an option:\n", "default");
                         input = scanChoice(new ArrayList<>(Arrays.asList(1, 2)));
 
                         switch (input) {
                             case 1: // Calculate
                                 Calculator c = new Calculator();
-                                boolean calculated = false;
                                 println("Please input your equation:\n", "blue");
                                 String in = scanner.nextLine();
                                 try {
@@ -64,7 +66,66 @@ public class CLI {
                         }
                     }
                     break;
-                case 2: // Base Translator
+                case 2:
+                    // ---------------
+                    // Base Translator
+                    // ---------------
+                    BaseTranslator bT = new BaseTranslator();
+                    input = 0;
+                    println("Welcome to the base translator interface! Choose an option:", "blue");
+                    println("(1) Calculate your inputs", "default");
+                    println("(2) Change input number and base", "default");
+                    println("(3 Change output base", "default");
+                    println("(4) Exit", "default");
+
+                    while (input != 4) {
+                        println("\nChoose an option:\n", "default");
+                        input = scanChoice(new ArrayList<>(Arrays.asList(1, 2, 3, 4)));
+
+                        switch (input) {
+                            case 1: // Calculate
+                                String output = "";
+                                try {
+                                    output = bT.Calculate();
+                                } catch (Exception e) {
+                                    output = "Ran into an unknown error!";
+                                }
+                                switch (output) {
+                                    case "Error! Not a valid character.":
+                                    case "Error! Cannot be a negative number.":
+                                    case "Error! Used a character with a higher value than the input base.":
+                                    case "Error! Something went wrong :(":
+                                    case "Error! Input Base must be in the range of 2 - 36.":
+                                    case "Ran into an unknown error!":
+                                        println(output, "red");
+                                        break;
+                                    default:
+                                        println(output, "default");
+                                        break;
+                                }
+                                break;
+                            case 2: // Set Inputs
+                                println("Please input your input number:\n", "default");
+                                bT.setInput(scanner.nextLine());
+                                println("\nPlease input your input base:\n", "default");
+                                try {
+                                    bT.setInBase(Integer.parseInt(scanner.nextLine()));
+                                } catch (Exception e) {
+                                    println("\nError! Invalid input base.", "red");
+                                }
+                                break;
+                            case 3: // Set Outbase
+                                println("Please input your outputs base:\n", "default");
+                                try {
+                                    bT.setOutBase(Integer.parseInt(scanner.nextLine()));
+                                } catch (Exception e) {
+                                    println("\nError! Invalid output base.", "red");
+                                }
+                                break;
+                            case 4: // Exit
+                                break;
+                        }
+                    }
                     break;
                 case 3: // Exit
                     System.exit(0);
